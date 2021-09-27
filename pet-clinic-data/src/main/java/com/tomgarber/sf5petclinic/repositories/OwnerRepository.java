f79d9a950e77b0be74588d25ca173cf79c3c8216
Tom Garber <thogarbe@amazon.com>
2021-09-27 10:30:30 -0500

    Added spring data jps service for owners.  Closes #40

--- pet-clinic-data/src/main/java/com/tomgarber/sf5petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/tomgarber/sf5petclinic/repositories/OwnerRepository.java
@@ -4,4 +4,6 @@ import com.tomgarber.sf5petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
